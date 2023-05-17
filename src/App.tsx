d044e7e4693ebac84b44e02ed495a24760f4cff2
im4k1r4 <imthiagomartins@live.com>
2023-05-17 20:41:11 -0300

    Iniciando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Importando estilos do módulo de css
+import styles from './App.module.css'
 
-function App() {
+// Importando imagem dos assets
+import poweredImage from './assets/powered.png'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150}/>
+        </div>
       </header>
+
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          ...
+        </div>
+        <div className={styles.rightSide}>
+          ...
+        </div>
+      </div>
     </div>
   );
 }
